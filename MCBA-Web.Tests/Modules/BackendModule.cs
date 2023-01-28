f1b4ab63b66f1d0f82801b5ec260fb4fff79e0a4
Abida <s3811920@student.rmit.edu.au>
2023-01-28 11:47:11 +1100

    register test classes

--- MCBA-Web.Tests/Modules/BackendModule.cs
+++ MCBA-Web.Tests/Modules/BackendModule.cs
@@ -21,11 +21,24 @@ namespace MCBA_Web.Tests.Modules
             // Register types for DI (Dependency Injection).
             builder.Register(c => new MCBAContext(new DbContextOptionsBuilder<MCBAContext>().
             UseInMemoryDatabase(nameof(MCBAContext)).Options));
-
+            
             builder.RegisterType<AccountService>().As<IAccountService>();
             builder.RegisterType<TransactionService>().As<ITransactionService>();
             builder.RegisterType<BillPayService>().As<IBillPayService>();
             builder.RegisterType<PayeeService>().As<IPayeeService>();
+            builder.RegisterType<AddressService>().As<IAddressService>();
+            builder.RegisterType<BillSchedulerService>().As<BillSchedulerService>();
+            builder.RegisterType<CustomerService>().As<ICustomerService>();
+            builder.RegisterType<LoginService>().As<ILoginService>();
+
+            builder.RegisterType<AccountController>().As<AccountController>();
+            builder.RegisterType<AddressController>().As<AddressController>();
+            builder.RegisterType<BillPayController>().As<BillPayController>();
+            builder.RegisterType<CustomerController>().As<CustomerController>();
+            builder.RegisterType<HomeController>().As<HomeController>();
+            builder.RegisterType<LoginController>().As<LoginController>();
+            builder.RegisterType<PayeeController>().As<PayeeController>();
+            builder.RegisterType<TransactionController>().As<TransactionController>();
         }
     }
 }
